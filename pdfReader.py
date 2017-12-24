9d0d9df8a065df7e84c3877b5117c467d0416856
Priya <prrani28@gmail.com>
2017-12-24 18:57:49 +0530

    Added speechToText.py script

--- pdfReader.py
+++ pdfReader.py
@@ -22,18 +22,31 @@ if n>1:
 	page = 'pages.'
 else:
 	page = 'page.'
-	
-speak.Speak('There are '+ n2w.num2words(n,to='cardinal') + page)
+while True:
+    try:
+	speak.Speak('There are '+ n2w.num2words(n,to='cardinal') + page)
 
-for i in range(n):
-	p = n2w.num2words(i+1, to='ordinal')
-	speak.Speak(p + 'page') 
-	# creating a page object
-	pageObj = pdfReader.getPage(i)
+	for i in range(n):
+		speak.Speak(n2w.num2words(i+1, to='ordinal') + 'page') 
+		# creating a page object
+		pageObj = pdfReader.getPage(i)
 	 
-	# extracting text from page
-	text = pageObj.extractText()
-	speak.Speak(text)
+		# extracting text from page
+		text = pageObj.extractText()
+		speak.Speak(text)
+
+        
+    except KeyboardInterrupt:
+        speak.Speak('\nPausing...  (Hit ENTER to continue, type quit to exit.)')
+        try:
+            response = raw_input()
+            if response == 'quit':
+                break
+            speak.Speak('Resuming...')
+        except KeyboardInterrupt:
+            speak.Speak('Resuming...')
+            continue	
+	
 
 # closing the pdf file object
 pdfFileObj.close()
