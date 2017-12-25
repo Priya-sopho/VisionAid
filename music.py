1e1f86d89219b54563a175416b9442b6be0d9e4b
Chavqueen <chavqueen.cs@gmail.com>
2017-12-26 02:36:21 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,4 +1,6 @@
+#pip install pygame
 import pygame as pg
+
 def play_music(music_file, volume=0.8):
     '''
     stream music with mixer.music module in a blocking manner
@@ -9,22 +11,31 @@ def play_music(music_file, volume=0.8):
     bitsize = -16 # unsigned 16 bit
     channels = 2 # 1 is mono, 2 is stereo
     buffer = 2048 # number of samples (experiment to get best sound)
+    
     #initializes the pygame mixer (player) with the given arguments
     pg.mixer.init(freq, bitsize, channels, buffer)
+    
     pg.mixer.music.set_volume(volume)
+    
     clock = pg.time.Clock()
+    
     try:
         pg.mixer.music.load(music_file)
         print("Music file {} loaded!".format(music_file))
+    
     except pg.error:
         print("File {} not found! ({})".format(music_file, pg.get_error()))
         return
     pg.mixer.music.play()
+    
     #is the pygame event loop
     while pg.mixer.music.get_busy():
         # check if playback has finished
         clock.tick(30)
-music_file = "C:\\Users\\H\\Music\\English Songs\\Cut - Copy.mp3"
+
+music_file = "C:\Users\H\Music\English Songs\Cut - Copy.mp3"
+
 # optional volume 0 to 1.0
 volume = 0.8
+
 play_music(music_file, volume)
