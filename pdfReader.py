80d7912d37d6b6382a5996cb5c3c367da6fbd04b
Priya <prrani28@gmail.com>
2017-12-25 17:31:57 +0530

    Added exit button in pdfReader

--- pdfReader.py
+++ pdfReader.py
@@ -1,11 +1,25 @@
 #pip install PyPDF2
 #pip install pypiwin32 or pywin32
 #pip install num2words
+#pip install speech
 # importing required modules
 import PyPDF2
-import win32com.client as wincl
+import speech
+#import win32com.client as wincl
 import num2words as n2w
-speak = wincl.Dispatch("SAPI.SpVoice")
+import os 
+#speak = wincl.Dispatch("SAPI.SpVoice")
+
+def callback(phrase, listener):
+	speech.say('You said '+phrase)
+	phrase = phrase.lower()
+	if phrase == "exit" :
+		speech.say('Exiting pdf file reading task')
+		os._exit(1)		
+			
+
+
+
 
 #Pdf file to listen
 file = 'C:\Users\Priya Rani\Desktop\Wedu\SOP.pdf' 
@@ -22,31 +36,27 @@ if n>1:
 	page = 'pages.'
 else:
 	page = 'page.'
-while True:
-    try:
-	speak.Speak('There are '+ n2w.num2words(n,to='cardinal') + page)
 
+
+response = speech.input("Say something, please.")
+speech.say("You said " + response)
+
+   
+
+speech.say('There are '+ n2w.num2words(n,to='cardinal') + page)
+listener = speech.listenforanything(callback)
+while listener.islistening():
 	for i in range(n):
-		speak.Speak(n2w.num2words(i+1, to='ordinal') + 'page') 
+		speech.say(n2w.num2words(i+1, to='ordinal') + 'page') 
 		# creating a page object
 		pageObj = pdfReader.getPage(i)
-	 
+		listener = speech.listenforanything(callback)
 		# extracting text from page
-		text = pageObj.extractText()
-		speak.Speak(text)
+		lines = pageObj.extractText().splitlines()
+		for each_line in lines:
+			speech.say(each_line)
+			listener = speech.listenforanything(callback)
 
-        
-    except KeyboardInterrupt:
-        speak.Speak('\nPausing...  (Hit ENTER to continue, type quit to exit.)')
-        try:
-            response = raw_input()
-            if response == 'quit':
-                break
-            speak.Speak('Resuming...')
-        except KeyboardInterrupt:
-            speak.Speak('Resuming...')
-            continue	
-	
 
 # closing the pdf file object
 pdfFileObj.close()
