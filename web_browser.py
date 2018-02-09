a7ec48003e611f33614115ea291fb20d0b6bede7
shalini06 <slini06@gmail.com>
2018-02-09 23:44:37 +0530

    Resolved conflicts

--- web_browser.py
+++ web_browser.py
@@ -68,7 +68,11 @@ class webBrowser:
 	#Bookmarks exported to a file "bookmarks"
 	def accessBookmarks(self):
 		import chrome_bookmarks as bookmarks
-		bookmarks.export()
+		bookmarks =  bookmarks.export()
+		#Relace with reader function
+		for bookmark in bookmarks:
+			print(bookmark['name']).encode('utf-8')
+			print(bookmark['url']).encode('utf-8')
 
 	#History exported to a file "history"
 	def accessHistory(self):
