b42bdad52ef970e833e4fcb58f5fae5691948ac6
Priya <prrani28@gmail.com>
2018-02-12 23:27:18 +0530

    Integrated word reader with key interrupts

--- pdfReader.py
+++ pdfReader.py
@@ -1,64 +1,407 @@
 #pip install PyPDF2
 #pip install pypiwin32 or pywin32
 #pip install num2words
-#pip install speech
+#pip install pynput
 # importing required modules
 import PyPDF2
-import speech
-#import win32com.client as wincl
+import win32com.client as wincl
 import num2words as n2w
 import os 
-#speak = wincl.Dispatch("SAPI.SpVoice")
+import re
+import threading,time
+voice = wincl.Dispatch("SAPI.SpVoice")
+from pynput import keyboard 
+import msvcrt
 
-def callback(phrase, listener):
-	speech.say('You said '+phrase)
-	phrase = phrase.lower()
-	if phrase == "exit" :
-		speech.say('Exiting pdf file reading task')
+
+#Pdf file to listen
+file = "C:\Users\Priya Rani\Desktop\Wedu\SOP.pdf" 
+
+"""
+ pdfReader to listen the content of pdf file
+ Following are the function alongwith keyboard keys to be pressed to execute:
+ 1. Pause/Resume - Space
+ 2. Change Speed - up key to increase speed, down key to decrease speed
+ 3. Find a keyword in file - f
+ 4. Jump on a specific page - j
+ 5. Rewind (Move 10 lines back) - left key
+ 6. Repeat - r
+ 7. Skip current page - right key
+ 8. Quit - Esc key or q 
+"""
+class pdfReader:
+	task = ['Start','Pause','Resume','Exit','Rewind','Repeat','Skip current page','Jump on a page',
+		'Search','Change speed']
+
+	def __init__(self, file,password = ''):
+			 
+		try:
+			# creating a pdf file object
+			self.pdfFileObj = open(file, 'rb') 
+				 
+			# creating a pdf reader object
+			self.pdfReader = PyPDF2.PdfFileReader(self.pdfFileObj)
+
+			#decrypting password
+			if password != '':
+				self.pdfReader.decrypt(password)
+		except:
+			voice.Speak('Unable to open the file')
+			os._exit(1)
+
+		#line no.
+		self._line = 0
+		#page no.
+		self._page = 0
+
+		# creating a page object
+		self.pageObj = self.pdfReader.getPage(self._page)
+			
+		# extracting text from page
+		self.lines = self.pageObj.extractText().splitlines()
+		
+		#Lock
+		self.lock = threading.Lock()  #Initially pause is on
+		
+		#Pause button
+		self.pause_key = 0
+
+		kb = threading.Thread(target=self.listenKeyboard)
+		kb.start()
+		
+
+	"""
+	 Destructor
+	"""    
+	def __del__(self):
+		self.pdfFileObj.close()
+
+	"""
+	 Keyboard listening callback function
+	"""
+	def on_press(self,key):
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
+			
+		try:
+			if key.char == 'q':
+				self.exit()
+			if key.char == 'j':
+			    self.jump()
+			elif key.char == 'f':
+			    self.search()
+			elif key.char == 'r':
+			    self.repeat() 
+		        
+		except AttributeError:
+			if (key == keyboard.Key.space) and self.pause_key:
+				self.pause_key = 0
+				self.lock.release()
+				    #self.resume()
+			elif key == keyboard.Key.space:
+				self.lock.acquire()
+				self.pause()
+			elif(key == keyboard.Key.up):
+			    self.change_speed(1)
+			elif(key == keyboard.Key.down):
+			    self.change_speed(-1)
+			elif(key == keyboard.Key.right):
+			    self.skip()
+			elif(key == keyboard.Key.left):
+			    self.rewind()
+			elif(key == keyboard.Key.esc):
+			    self.exit()
+
+	"""
+	 Listen to keyboard keys
+	"""
+	def listenKeyboard(self):
+	    # Collect events until released
+	    with keyboard.Listener(on_press=self.on_press) as listener:
+	        listener.join()
+
+	"""
+	 Helper function for search function 
+	 Takes input string to search in pdf file, 
+	 return page number of pdf file where input keyword is found.
+	"""
+	def  findText(self,s):
+		pages = []
+		s = re.compile(s.lower())
+		for i in range(self.pdfReader.getNumPages()):
+			content = self.pdfReader.getPage(i).extractText().lower()
+			if s.search(content) is not None:
+				if i not in pages:
+					pages.append(i)
+		
+		return pages
+
+	"""
+	 Menu to speak all the available task 
+	"""	
+	def menu(self):
+		voice.Speak('What do you want to do?')
+		for i in range(len(self.task)):
+			voice.Speak(n2w.num2words(i+1,to='ordinal')+self.task[i])
+		response = int(raw_input())
+		if response < len(self.task):
+			voice.Speak(self.task[response-1])
+		self.action(response)
+
+
+	
+	"""
+	 Start the pdf reading task.
+	"""
+	def start(self):
+		try:
+			
+			#Number of pages in pdf file
+			self.total_pages = self.pdfReader.numPages
+			if self.total_pages>1:
+				page = 'pages.'
+			else:
+				page = 'page.'
+
+			self.lock.acquire()	
+			voice.Speak('Welcome to pdf listening task')
+			voice.Speak('There are '+ n2w.num2words(self.total_pages,to='cardinal') + page)
+			self.lock.release()
+			self.resume()
+		except :
+			voice.Speak('Some Error')
+			return
+			
+
+	"""
+	  Pause pdf reading task
+	"""
+	def pause(self):
+		self.pause_key = 1
+		voice.Speak('Pausing')
+		
+		
+
+	"""
+	 Resume pdf task
+	"""
+	def resume(self):
+		#Wait until pause is not false
+		try:
+			if(self._page >= self.total_pages):
+				voice.Speak('We reached the end of file')
+				self.exit()
+			
+			#if on first line of page,then tell page number
+			if self._line == 0:
+				self.lock.acquire()
+				voice.Speak(n2w.num2words(self._page+1, to='ordinal') + 'page') 
+				self.lock.release()
+			
+			# creating a page object
+			self.pageObj = self.pdfReader.getPage(self._page)
+			
+			# extracting text from page
+			self.lines = self.pageObj.extractText().splitlines()
+			if len(self.lines) == 0:
+				voice.Speak('Unable to detect the content')
+			while self._line < len(self.lines):
+				self.lock.acquire()
+				voice.Speak(self.lines[self._line])
+				self._line += 1		
+				self.lock.release()
+			self._page += 1
+			self._line = 0
+			self.resume()
+
+		except:
+			voice.Speak("Some error")
+			return
+		
+	"""
+	 Exit pdf Reading Task
+	"""
+	def exit(self):		
+		voice.Speak('Exiting pdf reading task')
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
 		os._exit(1)
-	if phrase == "pause" :
-		speech.say('What do you want to do?')
-		#Menu
-		speech.say(n2w.num2words(1, to='ordinal') + 'Exit')
-		speech.say(n2w.num2words(2, to='ordinal') + 'Resume') 
-		listener = speech.listenforanything(callback)
+	
+
+	"""
+	  Repeat the last line
+	"""	
+	def repeat(self):
+		self.lock.acquire()
+		self._line -= 1
+		if(self._line<0):
+			self._line = 0
+			self._page -= 1
+			if(self._page < 0):
+				self._page = 0
+			#if on first line of page,then tell page number
+			if self._line == 0:
+				voice.Speak(n2w.num2words(self._page+1, to='ordinal') + 'page') 
+			
+			# creating a page object
+			self.pageObj = self.pdfReader.getPage(self._page)
+			
+			# extracting text from page
+			self.lines = self.pageObj.extractText().splitlines()
+			if len(self.lines) == 0:
+				voice.Speak('Unable to detect the content')
+		self.lock.release()
+			
+	
+	"""
+	 Move 10 lines back
+	"""
+	def rewind(self):
+		self.lock.acquire()
+		self._line -= 10
+		if(self._line<0):
+			self._line = 0
+			self._page -= 1
+			if(self._page < 0):
+				self._page = 0
+			# creating a page object
+			self.pageObj = self.pdfReader.getPage(self._page)
+			
+			# extracting text from page
+			self.lines = self.pageObj.extractText().splitlines()
+			
+			self._line = len(self.lines) - 10
+			if(self._line < 0):
+				self._line = 0
+			#if on first line of page,then tell page number
+			if self._line == 0:
+				voice.Speak(n2w.num2words(self._page+1, to='ordinal') + 'page') 
+			if len(self.lines) == 0:
+				voice.Speak('Unable to detect the content')
+		self.lock.release()
+	
+
+	"""
+	 Skip current and move to next page
+	"""
+	def skip(self):
+		self.lock.acquire()
+		voice.Speak('Moving to next page')
+		self._page += 1
+		self._line = 0
+		if(self._page >= self.total_pages):
+			voice.Speak('We reached the end of file')
+			self.exit()
+
+		#if on first line of page,then tell page number
+		if self._line == 0:
+			voice.Speak(n2w.num2words(self._page+1, to='ordinal') + 'page') 
+		
+		# creating a page object
+		self.pageObj = self.pdfReader.getPage(self._page)
+		
+		# extracting text from page
+		self.lines = self.pageObj.extractText().splitlines()
+		if len(self.lines) == 0:
+			voice.Speak('Unable to detect the content')
+		self.lock.release()
+		
+	
+
+	"""
+	 Jump on a specific page
+	"""		
+	def jump(self):
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
+		self.lock.acquire()
+		voice.Speak('Tell me the page number to jump on?')
+		self._page = int(raw_input())-1
+		if(self._page < 0):
+			self._page = 0
+		if(self._page > self.total_pages):
+			self._page = self.total_pages -1
+		if(self._page >= self.total_pages):
+			voice.Speak('We reached the end of file')
+			self.exit()
+		self._line = 0
+		#if on first line of page,then tell page number
+		if self._line == 0:
+			voice.Speak(n2w.num2words(self._page+1, to='ordinal') + 'page') 
+		
+		# creating a page object
+		self.pageObj = self.pdfReader.getPage(self._page)
+		
+		# extracting text from page
+		self.lines = self.pageObj.extractText().splitlines()
+		if len(self.lines) == 0:
+			voice.Speak('Unable to detect the content')
+		self.lock.release()
+		
+	
+
+	"""
+	 Search for a specific word in pdf file
+	"""
+	def search(self):
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
+		self.lock.acquire()
+		voice.Speak('What you want to search?')
+		r = raw_input()
+		pages = self.findText(r)
+		if len(pages) == 0:
+			voice.Speak(r+' not found!!')
+		else :	
+			if len(pages)>1:
+				page = 'pages.'
+			else:
+				page = 'page.'
+			voice.Speak(r+ ' found in '+ n2w.num2words(len(pages), to='cardinal') + page)
+			for p in pages:
+				voice.Speak('Found at page number'+ n2w.num2words(p+1,to='cardinal'))
+		self.lock.release()
+		
+		
+	"""
+	 Change the rate of speaking up key increase the speed, down key decrease the speed
+	"""
+	def change_speed(self,r):
+		voice.Rate += r
+	
+#Callback for speech recognizing	
+def Task(phrase):
+	phrase.lower()
+	if phrase == 'start':
+		start()
+	elif phrase == 'pause':
+		pause()
+	elif phrase == 'resume':
+		resume()
+	elif phrase == 'exit':
+		exit()
+	elif phrase == 'rewind':
+		rewind()
+	elif phrase == 'repeat':
+		repeat()
+	elif phrase == 'skip':
+		skip()
+	elif phrase == 'jump':
+		jump()
+	elif phrase == 'search':
+		search()
+	elif phrase == 'change speed':
+		change_speed()
+	else:
+		voice.Speak('Sorry! Unable to recognize your action')
+	
+	
+#start()
+reader = pdfReader(file)
+reader.start()		
 
 		
-#Pdf file to listen
-file = 'C:\Users\Priya Rani\Desktop\Wedu\SOP.pdf' 
- 
-# creating a pdf file object
-pdfFileObj = open(file, 'rb') 
-# creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
- 
-#Number of pages in pdf file
-n = pdfReader.numPages
-if n>1:
-	page = 'pages.'
-else:
-	page = 'page.'
-
-
-response = speech.input("Say something, please.")
-speech.say("You said " + response)
-
-   
-
-speech.say('There are '+ n2w.num2words(n,to='cardinal') + page)
-listener = speech.listenforanything(callback)
-while listener.islistening():
-	for i in range(n):
-		speech.say(n2w.num2words(i+1, to='ordinal') + 'page') 
-		# creating a page object
-		pageObj = pdfReader.getPage(i)
-		listener = speech.listenforanything(callback)
-		# extracting text from page
-		lines = pageObj.extractText().splitlines()
-		for each_line in lines:
-			speech.say(each_line)
-			listener = speech.listenforanything(callback)
-
-
-# closing the pdf file object
-pdfFileObj.close()
--- wordReader.py
+++ wordReader.py
@@ -7,41 +7,122 @@ import win32com.client as wincl
 import num2words as n2w
 import os 
 import re
-sense = wincl.Dispatch("SAPI.SpVoice")
+import threading,time
+voice = wincl.Dispatch("SAPI.SpVoice")
+from pynput import keyboard 
+import msvcrt
 
-#Pdf file to listen
+#Word file to listen
 file = 'C:\Users\Priya Rani\Desktop\Wedu\LDP.docx' 
 
+"""
+ wordReader to listen the content of word file
+ Following are the function alongwith keyboard keys to be pressed to execute:
+ 1. Pause/Resume - Space
+ 2. Change Speed - up key to increase speed, down key to decrease speed
+ 3. Find a keyword in file - f
+ 4. Jump on a specific para - j
+ 5. Rewind (Move 10 lines back) - left key
+ 6. Repeat - r
+ 7. Skip current para - right key
+ 8. Quit - Esc key or q 
+"""
 class wordReader:
-	#line no.
-	_line = 0
-	#para no.
-	_para = 0
 	task = ['Start','Pause','Resume','Exit','Rewind','Repeat','Skip current para','Jump on a para',
 		'Search','Change speed']
 
 	def __init__(self, file):
 		 
-		# creating a file object
-		self.FileObj = open(file, 'rb') 
-			 
-		# creating a document object
-		self.document = Document(self.FileObj)
+		try:
+			# creating a file object
+			self.FileObj = open(file, 'rb') 
+				 
+			# creating a document object
+			self.document = Document(self.FileObj)
+		except:
+			voice.Speak('Unable to open the file')
+			os._exit(1)
 
+		#line no.
+		self._line = 0
+		
+		#para no.
+		self._para = 0
+	
 		self.paragraphs = self.document.paragraphs
 	
 		self.paragraphs = [p for p in self.paragraphs if len(p.text) != 0]		 
+		
 		#Number of paras in doc file
 		self.total_paras = len(self.paragraphs)
-		if self.total_paras>1:
-			para = 'paragraphs.'
-		else:
-			para = 'paragraph.'
+		
+		# creating a para object
+		self.paraObj = self.paragraphs[self._para]
+		
+		# extracting text from para
+		self.lines = self.paraObj.text.splitlines()
+		
+		#Lock
+		self.lock = threading.Lock()  #Initially pause is on
+		
+		#Pause button
+		self.pause_key = 0
 
-		sense.Speak('There are '+ n2w.num2words(self.total_paras,to='cardinal') + para)
+		kb = threading.Thread(target=self.listenKeyboard)
+		kb.start()
+		
 
+	"""
+	 Destructor
+	"""
 	def __del__(self):
 	  	self.FileObj.close()
+
+	"""
+	 Keyboard listening Callback function
+	"""
+	def on_press(self,key):
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
+			
+		try:
+			if key.char == 'q':
+				self.exit()
+			if key.char == 'j':
+			    self.jump()
+			elif key.char == 'f':
+			    self.search()
+			elif key.char == 'r':
+			    self.repeat() 
+		        
+		except AttributeError:
+			if (key == keyboard.Key.space) and self.pause_key:
+				self.pause_key = 0
+				self.lock.release()
+				    #self.resume()
+			elif key == keyboard.Key.space:
+				self.lock.acquire()
+				self.pause()
+			elif(key == keyboard.Key.up):
+			    self.change_speed(1)
+			elif(key == keyboard.Key.down):
+			    self.change_speed(-1)
+			elif(key == keyboard.Key.right):
+			    self.skip()
+			elif(key == keyboard.Key.left):
+			    self.rewind()
+			elif(key == keyboard.Key.esc):
+			    self.exit()
+
+	"""
+	 Listen to keyboard keys
+	"""
+	def listenKeyboard(self):
+	    # Collect events until released
+	    with keyboard.Listener(on_press=self.on_press) as listener:
+	        listener.join()
+
 
 	def  findText(self,s):
 		paras = []
@@ -67,148 +148,226 @@ class wordReader:
 		self.action(response)
 
 
-	
+	"""
+	 Start the word reading task.
+	"""
 	def start(self):
 		try:
+			
+			if self.total_paras>1:
+				para = 'paragraphs.'
+			else:
+				para = 'paragraph.'
+
+			self.lock.acquire()
+			voice.Speak('Welcome to word listening task')
+			voice.Speak('There are '+ n2w.num2words(self.total_paras,to='cardinal') + para)
+			self.lock.release()
+			self.resume()
+		except :
+			voice.Speak('Some Error')
+			return
+	
+	
+	"""
+	  Pause word reading task
+	"""
+	def pause(self):
+		self.pause_key = 1
+		voice.Speak('Pausing')
+
+
+	"""
+	 Resume word task
+	"""
+	def resume(self):
+		#Wait until pause is not false
+		try:
 			if(self._para >= self.total_paras):
-				sense.speak('We reached the end of the doc file')
-				return 
-			sense.Speak(n2w.num2words(self._para+1, to='ordinal') + 'para') 
+				voice.Speak('We reached the end of the doc file')
+				self.exit()
+			
+			#if on first line of para,then tell para number
+			if self._line == 0:
+				self.lock.acquire()
+				voice.Speak(n2w.num2words(self._para+1, to='ordinal') + 'para') 
+				self.lock.release()
 			
 			# creating a para object
-			paraObj = self.paragraphs[self._para]
+			self.paraObj = self.paragraphs[self._para]
 			
 			# extracting text from para
-			lines = paraObj.text.splitlines()
-			while self._line < len(lines):
-				sense.Speak(lines[self._line])
+			self.lines = self.paraObj.text.splitlines()
+			
+			if len(self.lines) == 0:
+				voice.Speak('Unable to detect the content')
+			
+			while self._line < len(self.lines):
+				self.lock.acquire()
+				voice.Speak(self.lines[self._line])
 				self._line += 1		
+				self.lock.release()
 			self._para += 1
 			self._line = 0
-			self.start()
-		except KeyboardInterrupt:
-			sense.Speak('Pausing..')
-			response = int(raw_input())
-			if response < len(self.task):
-				sense.Speak(self.task[response-1])
-			self.action(response)
-			
+			self.resume()
 
-	def pause(self):
-		self.menu()
+		except:
+			voice.Speak("Some error")
+			return
+	
+	
 
-	def resume(self):
-		
-		if(self._para >= self.total_paras):
-				sense.speak('We reached the end of the doc file')
-				return 
-		
-		# creating a para object
-		paraObj = self.paragraphs[self._para]
-		
-		# extracting text from para
-		lines = paraObj.text.splitlines()
-		while self._line < len(lines):
-			sense.Speak(lines[self._line])
-			self._line += 1		
-		self._para += 1
-		self._line = 0
-		self.start() #begin with next para
-
+	"""
+	 Exit word Reading Task
+	"""
 	def exit(self):		
-		sense.Speak('Exiting word document reading task')
+		voice.Speak('Exiting word reading task')
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
 		os._exit(1)
 		
+	
+
+	"""
+	  Repeat the last line
+	"""	
 	def repeat(self):
-		
+		self.lock.acquire()
 		self._line -= 1
 		if(self._line<0):
 			self._line = 0
 			self._para -= 1
-		if(self._para < 0):
-			self._para = 0
-		self.resume()
-
+			if(self._para < 0):
+				self._para = 0
+			#if on first line of para,then tell para number
+			if self._line == 0:
+				voice.Speak(n2w.num2words(self._para+1, to='ordinal') + 'para') 
+			
+			# creating a para object
+			self.paraObj = self.paragraphs[self._para]
+			
+			# extracting text from para
+			self.lines = self.paraObj.text.splitlines()
+			if len(self.lines) == 0:
+				voice.Speak('Unable to detect the content')
+		self.lock.release()
+	
+	"""
+	 Move 10 lines back
+	"""
 	def rewind(self):
+		self.lock.acquire()
 		self._line -= 10
 		if(self._line<0):
 			self._line = 0
 			self._para -= 1
-		if(self._para < 0):
-			self._para = 0
-		self.resume()	
+			if(self._para < 0):
+				self._para = 0
+			# creating a para object
+			self.paraObj = self.paragraphs[self._para]
+			
+			# extracting text from para
+			self.lines = self.paraObj.text.splitlines()
+			
+			self._line = len(self.lines) - 10
+			if(self._line < 0):
+				self._line = 0
+			#if on first line of para,then tell para number
+			if self._line == 0:
+				voice.Speak(n2w.num2words(self._para+1, to='ordinal') + 'para') 
+			if len(self.lines) == 0:
+				voice.Speak('Unable to detect the content')
+		self.lock.release()
+	
 
-
+	"""
+	 Skip current and move to next para
+	"""
 	def skip(self):
-		sense.Speak('Moving to next paragraph')
+		self.lock.acquire()
+		voice.Speak('Moving to next para')
 		self._para += 1
 		self._line = 0
-		self.start()
+		if(self._para >= self.total_paras):
+			voice.Speak('We reached the end of file')
+			self.exit()
 
+		#if on first line of para,then tell para number
+		if self._line == 0:
+			voice.Speak(n2w.num2words(self._para+1, to='ordinal') + 'para') 
 		
+		# creating a para object
+		self.paraObj = self.paragraphs[self._para]
+			
+		# extracting text from para
+		self.lines = self.paraObj.text.splitlines()
+		if len(self.lines) == 0:
+			voice.Speak('Unable to detect the content')
+		self.lock.release()
+	
+		
+	"""
+	 Jump on a specific para
+	"""		
 	def jump(self):
-		sense.Speak('Tell me the para number to jump on?')
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
+		self.lock.acquire()
+		voice.Speak('Tell me the para number to jump on?')
 		self._para = int(raw_input())-1
 		if(self._para < 0):
 			self._para = 0
 		if(self._para > self.total_paras):
 			self._para = self.total_paras -1
-		self.start()
+		if(self._para >= self.total_paras):
+			voice.Speak('We reached the end of file')
+			self.exit()
+		self._line = 0
+		#if on first line of para,then tell para number
+		if self._line == 0:
+			voice.Speak(n2w.num2words(self._para+1, to='ordinal') + 'para') 
 		
+		# creating a para object
+		self.paraObj = self.paragraphs[self._para]
+			
+		# extracting text from para
+		self.lines = self.paraObj.text.splitlines()
+		if len(self.lines) == 0:
+			voice.Speak('Unable to detect the content')
+		self.lock.release()
+	
+	"""
+	 Search for a specific word in word file
+	"""
 	def search(self):
-		sense.Speak('What you want to search?')
+		#Clear the input flush
+		while msvcrt.kbhit():
+			msvcrt.getch()
+		self.lock.acquire()
+		voice.Speak('What you want to search?')
 		r = raw_input()
 		paras = self.findText(r)
 		if len(paras) == 0:
-			sense.Speak(r+' not found!!')
+			voice.Speak(r+' not found!!')
 		else :	
 			if len(paras)>1:
-				para = 'paragraphs.'
+				para = 'paras.'
 			else:
-				para = 'paragraph.'
-			sense.Speak(r+ ' found in '+ n2w.num2words(len(paras), to='cardinal') + para)
-		self.resume()	
+				para = 'para.'
+			voice.Speak(r+ ' found in '+ n2w.num2words(len(paras), to='cardinal') + para)
+			for p in paras:
+				voice.Speak('Found at para number'+ n2w.num2words(p+1,to='cardinal'))
+		self.lock.release()
 
-	def change_speed(self):
-		sense.Speak('Enter the rate change')
-		r = int(raw_input())
-		sense.Rate += r
-		sense.Speak('Changed rate by a factor of '+ n2w.num2words(r, to='cardinal'))
-		self.resume()
-    
-
-	def action(self,choice):
-		try:
-			if choice == 1:
-				self.start()
-			elif choice == 2:
-				self.pause()
-			elif choice == 3:
-				self.resume()
-			elif choice == 4:
-				self.exit()
-			elif choice == 5:
-				self.rewind()
-			elif choice == 6:
-				self.repeat()
-			elif choice == 7:
-				self.skip()
-			elif choice == 8:
-				self.jump()
-			elif choice == 9:
-				self.search()
-			elif choice == 10:
-				self.change_speed()
-			else:
-				sense.Speak('Wrong Input!!')
-				self.menu()
-		except KeyboardInterrupt:
-			sense.Speak('Pausing..')
-			response = int(raw_input())
-			if response < len(self.task):
-				sense.Speak(self.task[response-1])
-			self.action(response)
-
+	"""
+	 Change the rate of speaking up key increase the speed, down key decrease the speed
+	"""
+	def change_speed(self,r):
+		voice.Rate += r
+	
+	
 def Task(phrase):
 	phrase.lower()
 	if phrase == 'start':
@@ -232,8 +391,8 @@ def Task(phrase):
 	elif phrase == 'change speed':
 		change_speed()
 	else:
-		sense.Speak('Sorry! Unable to recognize your action')
-	resume()
+		voice.Speak('Sorry! Unable to recognize your action')
+	
 	
 #start()
 reader = wordReader(file)
