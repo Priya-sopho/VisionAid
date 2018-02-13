5d475bc3c4254716be510dc882a040ad539c97fe
shalini06 <slini06@gmail.com>
2018-02-13 21:59:34 +0530

    Trial

--- web_browser.py
+++ web_browser.py
@@ -37,27 +37,38 @@ class webBrowser:
 
 	#function to run google search from command line
 	def googleSearch(self):
-		import webbrowser as wb
-		from google import search
-		from urlparse import urlparse
+		# import webbrowser as wb
+		# from google import search
+		# from urlparse import urlparse
 
-		urllist = []
-		query = listen(2048,"Google Search Keyword?")
+		# urllist = []
+		# # query = listen(2048,"Google Search Keyword?")
+		# query = raw_input()
+		# print query
+		# for j in search(query, tld="co.in", num=9, stop=1, pause=2):
+		# 	urllist.append(j)
+
+		# for i in range(0,len(urllist)):
+		# 	url = urlparse(urllist[i])
+		# 	output_text = url.netloc+url.path
+		# 	#SPEAK LOUD
+		# 	print output_text
+		# 	print "Visit this URL? (Yes/No)"
+		# 	ans = listen(256)
+		# 	print ans
+		# 	if(ans=='yes'):
+		# 		wb.open_new(urllist[i])
+		# 		break
+		#CSE ID = 005367157485527424859:9_sflux5spi
+		#API KEY= AIzaSyCijhKn3qzxMnEO2VplN7e7pL2vfJ3--BU
+		query = raw_input()
 		print query
-		for j in search(query, tld="co.in", num=9, stop=1, pause=2):
-			urllist.append(j)
-
-		for i in range(0,len(urllist)):
-			url = urlparse(urllist[i])
-			output_text = url.netloc+url.path
-			#SPEAK LOUD
-			print output_text
-			print "Visit this URL? (Yes/No)"
-			ans = listen(256)
-			print ans
-			if(ans=='yes'):
-				wb.open_new(urllist[i])
-				break
+		from googlesearch.googlesearch import GoogleSearch
+		response = GoogleSearch().search(query,num_results=10)
+		for result in response.results:
+		    print("Title: " + result.title)
+		    print("URL: " + result.url)
+		    print("END-----")
 
 	#Create bookmarks, Web browser must be open and active
 	def createBookmark(self):
@@ -80,8 +91,7 @@ class webBrowser:
 		history.export()
 
 instance = webBrowser()
-# instance.googleSearch()
-instance.accessBookmarks()
-instance.accessHistory()
+instance.googleSearch()
+
 
 
