d696501ff537cb516700d04e3ab30f9dd16922e4
mansi2301 <guptamansi298@gmail.com>
2018-02-19 18:07:31 +0530

    Added the delete a paragraph feature and some voice commands wherever missing

--- ms_word_document.py
+++ ms_word_document.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from docx import Document
+import re
 from docx.shared import Pt,Inches
 from docx.enum.style import WD_STYLE_TYPE
 import threading
@@ -7,7 +8,6 @@ import os
 import win32com.client as wincl
 from pynput import keyboard
 voice = wincl.Dispatch("SAPI.SpVoice")
-
 
 class docEditor:
 	def __init__(self):
@@ -17,7 +17,12 @@ class docEditor:
 		if '.docx' not in filename:
 			filename = filename + '.docx'
 		self.filename = filename
-		self.doc = Document(filename)
+		try:
+			self.doc = Document(filename)
+		except:
+			voice.Speak('The document cannot be created due to some error.Try to use any other document name!')
+			os.exit(1)
+		
 		self.para = self.doc.add_paragraph('')
 
 		kb = threading.Thread(target=self.listenKeyboard)
@@ -46,7 +51,6 @@ class docEditor:
 
 # Enter the text to be added
 	def enterText(self):
-		# text = raw_input("Enter the text : ")
 		voice.Speak("Enter the text: ")
 		lines = []
 		while True:
@@ -67,30 +71,33 @@ class docEditor:
 # add formatted text in the running paragraph
 	def formattedtext(self,formats):
 		types = formats.split(' ')
-		voice.Speak("Do u want to enter formatted text on a new line? (Y or N): ")
+		voice.Speak("Do u want to enter formatted text on a new line? Yes or No : ")
 		lineBreak = raw_input().upper()
 		run = self.para.add_run()
-		style = self.para.style
-		font  = style.font
+		text = self.enterText()
+		run.add_text(text) 
+		# style = self.para.style
+		# font  = style.font
 		if lineBreak == 'Y' or lineBreak == 'YES':
 			run.add_break()
 		if 'BOLD' or 'B' in types:
-			font.bold = not font.bold
-			run.bold = font.bold
+			# font.bold = not font.bold
+			run.bold = True
 		if 'ITALIC' or 'I' in types:
-			font.italic = not font.italic
-			run.italic = font.italic
+			# font.italic = not font.italic
+			run.italic = True
 		if 'UNDERLINE' or 'U' in types:
-			font.underline = not font.underline
-			run.underline = font.underline
-		text = self.enterText()
-		run.add_text(text)
+			# font.underline = not font.underline
+			run.underline = True
+		
 		self.saveAsDoc()
 
 # to make a new table 
 	def makeTable(self):
-		row_count = int(raw_input("Enter the following" + '\n' + 'Rows: '))
-		col_count = int(raw_input("Columns: "))
+		voice.Speak("Enter the following" + '\n' + 'Rows: ')
+		row_count = int(raw_input())
+		voice.Speak("Columns: ")
+		col_count = int(raw_input())
 		table = self.doc.add_table(row_count,col_count)
 		table.style.name = 'TableGrid'
 		voice.Speak("Enter the values in the table row by row: ")
@@ -115,38 +122,58 @@ class docEditor:
 
 # to add a heading to the document
 	def addHeading(self):
-		l = int(raw_input("Enter the heading level: "))
+		voice.Speak("Enter the heading level: ")
+		l = int(raw_input())
 		text = self.enterText()
 		self.doc.add_heading(text, level = l)
 		self.saveAsDoc()
 
 # to add a picture to the doc file
 	def addPicture(self):
-		pic_name = raw_input("Enter the name of the Picture (with extension i.e. .png or .jpeg): ")
-		pic_width = float(raw_input("Enter the picture width in inches: "))
+		voice.Speak("Enter the name of the Picture (with extension i.e. .png or .jpeg): ")
+		pic_name = raw_input()
+		voice.Speak("Enter the picture width in inches: ")
+		pic_width = float(raw_input())
 		pic = self.doc.add_picture(pic_name, width = Inches(pic_width))
 		self.saveAsDoc()
 
+#to find the paragraph with the particular word
+	def search_first_para(self):
+		
+		voice.Speak("Enter the word: ")
+		word = raw_input()
+		word = re.compile(word,re.I)
+		for paras in self.doc.paragraphs:
+			if word.search(paras.text) is not None:
+				return paras
+
+#to delete a particular paragraph from the document
+	def del_para(self,paragraph):
+		p = paragraph._element
+		p.getparent().remove(p)
+		p._p = p._element = None
+		self.saveAsDoc()
 
 	def menu(self):
 		Flag = True
 		while Flag == True:
 			voice.Speak('Do you want to listen to menu? (Y or N)')
-			ch = raw_input().upper()
-			if(ch == 'Y' or ch == 'Yes'):
+			choice = raw_input().upper()
+			if(choice == 'Y' or choice == 'YES'):
 				voice.Speak('1. To read the document' + '\n' +
 						'2. To add a Heading' + '\n' +
 						'3. To write a new paragraph on the document' + '\n' +
 						'4. To add a new formatted text' +'\n' +
 						'5. To change the font style and font name of the document' + '\n'
 						'6. To add a new Table and then display it' + '\n'
-						'7. To add Picture to the document ')
+						'7. To add Picture to the document ' + '\n' 
+						'8. To delete a paragraph containing a particular word ')
 			voice.Speak("Enter your choice number: ")
 			try:
 				ch = int(raw_input())
 				flag2 = True
 			except:
-				voice.Speak('Oops you have not entered a number')
+				voice.Speak('Oops! you have not entered a number.')
 
 			if flag2:
 				if ch == 1:
@@ -166,7 +193,7 @@ class docEditor:
 						if Continue == 'N' or Continue == 'NO':
 							flag2 = False
 				elif ch == 5:
-					voice.Speak("Enter the font name and font size(space seperated):")
+					voice.Speak("Enter the font name and font size (space seperated):")
 					name, size = raw_input().split(' ')
 					size = int(size)
 					self.changeDocFont(name, size)
@@ -174,8 +201,10 @@ class docEditor:
 					self.makeTable()
 				elif ch == 7:
 					self.addPicture()
+				elif ch == 8:
+					self.del_para(self.search_first_para())
 				else:
-					voice.Speak('Invalid chice')
+					voice.Speak('Invalid choice')
 			voice.Speak("Do You want to perform another operation? y or n: ")		
 			another_operation = raw_input().upper()
 			if another_operation == 'N' or another_operation == 'No':
