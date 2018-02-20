84dae80de8725d108d2e6572b7a7e5b64d8e8e14
shalini06 <slini06@gmail.com>
2018-02-20 23:39:20 +0530

    Keyboard Interrupts

--- web_browser.py
+++ web_browser.py
@@ -4,6 +4,7 @@
 	#2.Google Search
 	# pip install beautifulsoup4
 	# pip install google
+	# pip install pynput
 	#3.GUI 
 	# pip install pyautogui
 
@@ -18,36 +19,59 @@
 	7. Google Search - f
 """
 import threading,time 
-import msvcrt
+# import msvcrt
 from pynput import keyboard
 import os
 import webbrowser as web
 import io,json
 import datetime
+import termios, fcntl, sys, os
 
 class webBrowser:
 	def __init__(self):
+		print "Welcome to Web Browsing"
 		self.say("Welcome to Web Browsing")
-		kb = threading.Thread(target=self.listenKeyboard)
-		self.url = ""
-
+		self.lock = threading.Lock()
+		self.onPress()
+		kb = threading.Thread(target=self.listenKeyboard())
+		kb.start()
+		
 	def listenKeyboard(self):
-		with keyboard.Listener(on_press=self.onPress) as listener:
+		with keyboard.Listener(on_press=self.onPress()) as listener:
 			listener.join()
 
-	def onPress(self,key):
-		while msvcrt.kbhit():
-			msvcrt.getch()
+	def onPress(self):
+		fd = sys.stdin.fileno()
+		oldterm = termios.tcgetattr(fd)
+		newattr = termios.tcgetattr(fd)
+		newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+		termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+		oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+		fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
 		try:
-			if key.char == 'q':
-				self.exit()
-			if key.char == 'b':
-				self.createBookmark()
-			if key.char == 'f'
-				self.googleSearch()
-		except AttributeError:
-			if key == keyboard.Key.space:
-				self.visitUrl()
+			while (1):
+				try:
+					c = sys.stdin.read(1)
+					print "Got character", repr(c)
+					try:
+						if c == 'q':
+							self.exit()
+						if c == 'b':
+							self.createBookmark()
+						if c == 'f':
+							self.googleSearch()
+						if c == ' ':
+							self.visitUrl()
+								
+					except AttributeError:
+						print "Attribute Error"
+				except IOError: pass
+		finally:
+			termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+			fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+		
 
 
 	def say(self,line):
@@ -79,57 +103,59 @@ class webBrowser:
 
 	def exit(self):
 		self.say("Exiting now")
-		while msvcrt.kbhit():
-			msvcrt.getch()
+		sys.stdout.flush()
+		termios.tcflush(sys.stdin, termios.TCIOFLUSH)
 		os._exit(1)
 
-	def visitUrl(self):
-		self.lock.acquire()
-		self.say("Visiting chosen result")
-		web.open(self.result.link)
-		createHistory()
-		self.lock.release()
-
-	def googleSearch(self):
-		from google import google as web
-		self.lock.acquire()
-		self.say("Enter keywords to search")
-		keyWords = raw_input()
-		results = web.search(keyWords,1)
-		self.lock.release()
-
-		for self.result in results:
-			self.lock.acquire()
-			print "Name: ",self.result.name
-			self.say(self.result.name)
-			print "Description: ",self.result.description
-			self.say(self.result.description)
-			self.lock.release()
-			print "----------------"
-
-
-	#Create bookmarks, Web browser must be open and active
 	def createBookmark(self):
 		b = {}
 		b["name"] = self.result.name
 		b["url"] = self.result.link
 		b["datetime"] = str(datetime.datetime.now())
 		with open('bookmarks.txt', 'a+') as f:
-  			json.dump(b, f, ensure_ascii=False)
+			json.dump(b, f, ensure_ascii=False)
 
 	#Bookmarks exported to a file "bookmarks"
 	# def accessBookmarks(self):
-	def createHistory():
+	def createHistory(self):
+		print "history"
 		h = {}
 		h["name"] = self.result.name
 		h["url"] = self.result.link
 		h["datetime"] = str(datetime.datetime.now())
 		with open('history.txt', 'a+') as f:
-  			json.dump(h, f, ensure_ascii=False)
+			json.dump(h, f, ensure_ascii=False)
+
+	def visitUrl(self):
+		self.lock.acquire()
+		self.say("Visiting chosen result")
+		web.open(self.result.link)
+		self.createHistory()
+		self.lock.release()
+
+	def googleSearch(self):
+		from google import google
+		self.lock.acquire()
+		print "Enter keywords to search"
+		self.say("Enter keywords to search")
+		keyWords = raw_input()
+		results = google.search(keyWords,1)
+		self.lock.release()
+
+		for self.result in results:
+			print "Name: ",self.result.name
+			self.say(self.result.name.encode('utf-8'))
+			description = self.result.description.encode('utf-8')
+			print "Description: ",description
+			self.say("Description: "+description)
+			print "------------------------"
+			time.sleep(3)
+
+	
 		
 
-  	def accessHistory(self):
-	def accessHistory(self):
+ # def accessBookmarks(self):
+#def accessHistory(self):
 		
 
 
