656aa1e7304ac902dc3d0b0a0c85ed19987243bb
mansi2301 <guptamansi298@gmail.com>
2018-02-21 19:15:29 +0530

    added input flush and some try and except cases

--- ms_word_document.py
+++ ms_word_document.py
@@ -21,7 +21,7 @@ class docEditor:
 			self.doc = Document(filename)
 		except:
 			voice.Speak('The document cannot be created due to some error.Try to use any other document name!')
-			os.exit(1)
+			self.exit()
 		
 		self.para = self.doc.add_paragraph('')
 
@@ -38,6 +38,11 @@ class docEditor:
 	def exit(self):
 		os._exit(1)
 
+#clear the input flush
+	def clear_input(self):
+		while msvcrt.kbhit():
+			msvcrt.getch()
+
 # to get the whole document text for read operation
 	def getFullText(self):
 		fulltext = []
@@ -52,6 +57,7 @@ class docEditor:
 # Enter the text to be added
 	def enterText(self):
 		voice.Speak("Enter the text: ")
+		self.clear_input()
 		lines = []
 		while True:
 			line = raw_input()
@@ -72,6 +78,7 @@ class docEditor:
 	def formattedtext(self,formats):
 		types = formats.split(' ')
 		voice.Speak("Do u want to enter formatted text on a new line? Yes or No : ")
+		self.clear_input()
 		lineBreak = raw_input().upper()
 		run = self.para.add_run()
 		text = self.enterText()
@@ -95,12 +102,15 @@ class docEditor:
 # to make a new table 
 	def makeTable(self):
 		voice.Speak("Enter the following" + '\n' + 'Rows: ')
+		self.clear_input()
 		row_count = int(raw_input())
 		voice.Speak("Columns: ")
+		self.clear_input()
 		col_count = int(raw_input())
 		table = self.doc.add_table(row_count,col_count)
 		table.style.name = 'TableGrid'
 		voice.Speak("Enter the values in the table row by row: ")
+		self.clear_input()
 		for row in table.rows:
 			for cell in row.cells:
 				cell.text = raw_input()	
@@ -123,6 +133,7 @@ class docEditor:
 # to add a heading to the document
 	def addHeading(self):
 		voice.Speak("Enter the heading level: ")
+		self.clear_input()
 		l = int(raw_input())
 		text = self.enterText()
 		self.doc.add_heading(text, level = l)
@@ -131,16 +142,18 @@ class docEditor:
 # to add a picture to the doc file
 	def addPicture(self):
 		voice.Speak("Enter the name of the Picture (with extension i.e. .png or .jpeg): ")
+		self.clear_input()
 		pic_name = raw_input()
 		voice.Speak("Enter the picture width in inches: ")
+		self.clear_input()
 		pic_width = float(raw_input())
 		pic = self.doc.add_picture(pic_name, width = Inches(pic_width))
 		self.saveAsDoc()
 
 #to find the paragraph with the particular word
 	def search_first_para(self):
-		
 		voice.Speak("Enter the word: ")
+		self.clear_input()
 		word = raw_input()
 		word = re.compile(word,re.I)
 		for paras in self.doc.paragraphs:
@@ -152,12 +165,14 @@ class docEditor:
 		p = paragraph._element
 		p.getparent().remove(p)
 		p._p = p._element = None
+		self.para = self.doc.paragraphs[-1]
 		self.saveAsDoc()
 
 	def menu(self):
 		Flag = True
 		while Flag == True:
 			voice.Speak('Do you want to listen to menu? (Y or N)')
+			self.clear_input()
 			choice = raw_input().upper()
 			if(choice == 'Y' or choice == 'YES'):
 				voice.Speak('1. To read the document' + '\n' +
@@ -167,9 +182,11 @@ class docEditor:
 						'5. To change the font style and font name of the document' + '\n'
 						'6. To add a new Table and then display it' + '\n'
 						'7. To add Picture to the document ' + '\n' 
-						'8. To delete a paragraph containing a particular word ')
+						'8. To delete a paragraph containing a particular word ' + '\n' 
+						'9. To delete the last paragraph ')
 			voice.Speak("Enter your choice number: ")
 			try:
+				self.clear_input()
 				ch = int(raw_input())
 				flag2 = True
 			except:
@@ -177,32 +194,67 @@ class docEditor:
 
 			if flag2:
 				if ch == 1:
-					voice.Speak(self.getFullText())    # to display the text
+					try:
+						voice.Speak(self.getFullText())    # to display the text
+					except:
+						voice.Speak("Error in reading the document!")
 				elif ch == 2:
-					self.addHeading()
+					try:
+						self.addHeading()
+						voice.Speak("Heading added!")
+					except:
+						voice.Speak("Error in adding the heading!")
 				elif ch == 3:
-					self.addNewPara()
+					try:
+						self.addNewPara()
+						voice.Speak("Paragraph added!")
+					except:
+						voice.Speak("Error in adding new Paragraph!")
 				elif ch == 4:
 					while flag2 == True:
-						new_format = ''
-						voice.Speak('Enter your choice: bold(B), italic(I) or underline(U) (seperated by spaces): ')
-						new_format = raw_input().upper()
-						self.formattedtext(new_format)
-						voice.Speak("Do You want to add more formatted text? y or n: ")
-						Continue = raw_input().upper()
-						if Continue == 'N' or Continue == 'NO':
-							flag2 = False
+						try:
+							new_format = ''
+							voice.Speak('Enter your choice: bold(B), italic(I) or underline(U) (seperated by spaces): ')
+							self.clear_input()
+							new_format = raw_input().upper()
+							self.formattedtext(new_format)
+							voice.Speak("Do You want to add more formatted text? y or n: ")
+							self.clear_input()
+							Continue = raw_input().upper()
+							if Continue == 'N' or Continue == 'NO':
+								flag2 = False
+						except:
+							voice.Speak("Error in adding formatted text!")
 				elif ch == 5:
 					voice.Speak("Enter the font name and font size (space seperated):")
+					self.clear_input()
 					name, size = raw_input().split(' ')
 					size = int(size)
 					self.changeDocFont(name, size)
 				elif ch == 6:
-					self.makeTable()
+					try:
+						self.makeTable()
+						voice.Speak("Table added!")
+					except:
+						voice.Speak("Error in adding table!")
 				elif ch == 7:
-					self.addPicture()
+					try:
+						self.addPicture()
+						voice.Speak("Picture added!")
+					except:
+						voice.Speak("Error in adding picture!")
 				elif ch == 8:
-					self.del_para(self.search_first_para())
+					try:
+						self.del_para(self.search_first_para())
+						voice.Speak("Paragraph deleted!")
+					except:
+						voice.Speak("Error in deleting paragraph!")
+				elif ch == 9:
+					try:
+						self.del_para(self.para)
+						voice.Speak("Last Paragraph deleted!")
+					except:
+						voice.Speak("Error in deleting paragraph!")
 				else:
 					voice.Speak('Invalid choice')
 			voice.Speak("Do You want to perform another operation? y or n: ")		
